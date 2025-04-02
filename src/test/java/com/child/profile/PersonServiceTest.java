3aa282d9483722c180260270cab4be4ab345efbc
compurat <compurat@gmail.com>
2025-04-02 13:34:02 +0200

    initial commit

--- src/test/java/com/child/profile/PersonServiceTest.java
+++ src/test/java/com/child/profile/PersonServiceTest.java
@@ -16,7 +16,6 @@ import java.util.Date;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
